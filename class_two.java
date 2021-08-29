fe1032728cf1606eb894be076ae3c750716afe36
Khondokar Imtiaz Ahmed <82713315+K-Imtiaz@users.noreply.github.com>
2021-08-29 22:45:42 +0600

    Add files via upload

---
+++ class_two.java
@@ -0,0 +1,10 @@
+public class class_two {
+    public static void main(String[] args) {
+        Teacher teacher1=new Teacher("Imtiaz Ahmed","male",1742691950);
+        teacher1.displayInformation();
+        Teacher teacher2=new Teacher("Mahanaj Hoque","female",1923838488);
+        teacher2.displayInformation();
+
+
+    }
+    }
